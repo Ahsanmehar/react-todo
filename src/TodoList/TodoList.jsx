172d7f4f18b659fa43846e8227832ce151864130
Ahsanmehar <ahsanmeharj@gamil.com>
2024-12-19 01:42:54 +0500

    change

---
+++ src/TodoList/TodoList.jsx
@@ -0,0 +1,199 @@
+import { useState } from "react";
+import "./TodoList.css";
+import { v4 as uuidv4 } from "uuid";
+
+function TodoList() {
+  let [todo, setTodo] = useState([
+    { task: "Sample Todo", id: uuidv4(), isDone: false },
+  ]);
+  let [newTodo, setNewTodo] = useState("");
+
+
+
+  function handleClick() {
+    if (newTodo.trim()) {
+      setTodo((preval) => {
+        return [...preval, { task: newTodo, id: uuidv4(), isDone: false }];
+      });
+    } else {
+      console.log("hye");
+    }
+    setNewTodo("");
+  }
+
+  function handleNew(e) {
+    setNewTodo(e.target.value);
+  }
+
+  function handleDel(id) {
+    setTodo((preval) => {
+      return preval.filter((del) => {
+        return del.id != id;
+      });
+    });
+  }
+
+  function handleUppercase(id) {
+    setTodo((preval) => {
+      return preval.map((upper) => {
+        if (upper.id == id) {
+          return {
+            ...upper,
+            task: upper.task.toUpperCase(),
+          };
+        } else {
+          return upper;
+        }
+      });
+    });
+  }
+
+  function handleLowercase(id) {
+    setTodo((preval) => {
+      return preval.map((lower) => {
+        if (lower.id == id) {
+          return {
+            ...lower,
+            task: lower.task.toLowerCase(),
+          };
+        } else {
+          return lower;
+        }
+      });
+    });
+  }
+
+  function handleAllUppercase() {
+    setTodo((preval) => {
+      return preval.map((allupper) => {
+        return {
+          ...allupper,
+          task: allupper.task.toUpperCase(),
+        };
+      });
+    });
+  }
+
+  function handleAllLowercase() {
+    setTodo((preval) => {
+      return preval.map((alllower) => {
+        return {
+          ...alllower,
+          task: alllower.task.toLowerCase(),
+        };
+      });
+    });
+  }
+
+  function handleIsDone(id) {
+    setTodo((preval) => {
+      return preval.map((done) => {
+        if (done.id == id) {
+          return {
+            ...done,
+            isDone: true,
+          };
+        } else {
+          return done;
+        }
+      });
+    });
+  }
+
+  let [edit, setEdit] = useState("");
+
+  
+  function handleEdit(id) {
+    setTodo((preval) => {
+      return preval.map((todoedit) => {
+        if (todoedit.id == id) {
+          return {
+            ...todoedit,
+            task: edit,
+            isDone: false,
+          };
+        } else {
+          return todoedit;
+        }
+      });
+    });
+  }
+
+  function handleNewEdit(e) {
+    setEdit(e.target.value);
+  }
+
+
+  return (
+    <main>
+      <div id="todolist">
+        <div id="heading">
+          <h1>React Todo</h1>
+          <p>A simple React Todo List App</p>
+        </div>
+
+        <ol id="newtodo-box">
+          {todo.map((todo) => {
+            return (
+              <div id="newtodo" key={todo.id}>
+                <li>
+                  {todo.isDone ? (
+                    <div>
+                      <input
+                        type="text"
+                        placeholder="Enter Edit Task"
+                        onChange={handleNewEdit}
+                        value={edit}
+                      />
+                      <button onClick={() => handleEdit(todo.id)}>Save</button>
+                    </div>
+                  ) : (
+                    todo.task
+                  )}
+                </li>
+
+                {todo.isDone ? (
+                  ""
+                ) : (
+                  <div id="icons">
+                    <i
+                      className="ri-arrow-up-circle-line"
+                      onClick={() => handleUppercase(todo.id)}
+                    ></i>
+                    <i
+                      className="ri-arrow-down-circle-line"
+                      onClick={() => handleLowercase(todo.id)}
+                    ></i>
+                    <i
+                      className="ri-pencil-fill"
+                      onClick={() => handleIsDone(todo.id, todo.isDone)}
+                    ></i>
+
+                    <i
+                      className="ri-delete-bin-line"
+                      onClick={() => handleDel(todo.id)}
+                    ></i>
+                  </div>
+                )}
+              </div>
+            );
+          })}
+        </ol>
+
+        <div id="input">
+          <input
+            type="text"
+            placeholder="Write something"
+            value={newTodo}
+            onChange={handleNew}
+          />
+          <button onClick={handleClick}>Add Task</button>
+          <button onClick={handleAllUppercase}>All UpperCase</button>
+          <button onClick={handleAllLowercase}>All LowerCase</button>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default TodoList;
